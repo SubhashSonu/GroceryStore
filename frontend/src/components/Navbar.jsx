43a6d6a2e212f2ea4dfdd82b9408230763791a05
Subhash Kumar <subhashsonu7660@gmail.com>
2025-10-07 19:50:29 +0530

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react'
 import { navbarStyles } from '../assets/dummyStyles'
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png'
+import { navItems } from '../assets/Dummy';
+import { FiUser } from 'react-icons/fi';
+
 
 function Navbar() {
     const location = useLocation();
+    const navigate = useNavigate();
 
     const [scrolled, setScrolled] = useState(false);
     const [activeTab, setActiveTab] = useState(location.pathname);
+    const [isOpen, setIsOpen] = useState(false);
+    const [cartBounce , setCartBounce] = useState(false);
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        Boolean(localStorage.getItem('authToken'))
+    );
+    // Define Logout Function
+    const handleLogout = ()=>{
+        localStorage.removeItem('authToken')
+        localStorage.removeItem('userData')
+        localStorage.clear()
+        window.dispatchEvent(new Event('authStateChanged'))
+        navigate('/')
+
+    }
+
   return (
     <nav className={`${navbarStyles.nav}
      ${scrolled ? navbarStyles.scrolledNav : navbarStyles.unscrolledNav}`}>
@@ -43,8 +63,52 @@ function Navbar() {
                         className={`${navbarStyles.navItem}
                             ${activeTab === item.path ?
                          navbarStyles.activeNavItem 
-                         : navbarStyles.inactiveNavItem}`}></Link>
+                         : navbarStyles.inactiveNavItem}`}>
+                            <div className='flex items-center'>
+                                <span className={`${navbarStyles.navIcon}
+                                    ${activeTab === item.path ? 
+                                    navbarStyles.activeNavIcon 
+                                    : navbarStyles.inactiveNavIcon}`}>
+                                        {item.icon}
+                                </span>
+                                <span>{item.name}</span>
+                            </div>
+                            <div className={`${navbarStyles.navIndicator}
+                                ${activeTab == item.path ? 
+                            navbarStyles.activeIndicator : navbarStyles.inactiveIndicator}
+                                `}/>
+
+    
+                         </Link>
                     ))}
+
+                </div>
+
+                {/* {MOBILE HAMBURGER} */}
+                <div className={navbarStyles.iconsContainer}>
+                   {
+                    isLoggedIn ? (
+                        <button onClick={handleLogout}
+                        className={navbarStyles.loginLink}
+                        aria-label='Logout'>
+                            
+                            <FiUser className={navbarStyles.loginIcon} />
+                            <span className='ml-1 text-white'>Logout</span>
+                        </button>
+                    ) : (
+                        <Link to='/login' className={navbarStyles.loginLink}>
+                            <FiUser className={navbarStyles.loginIcon}/>
+                            <span className='ml-1 text-white'>Login</span>
+                        </Link>
+                    )
+                   }
+
+                   <Link to='/cart' className={navbarStyles.cartLink}>
+                   <FaCart className={`${navbarStyles.cartIcon} ${cartBounce ? 
+                    'animate-bounce' : ''
+                   }`}/>
+                   
+                   </Link>
                 </div>
             </div>
         </div>
