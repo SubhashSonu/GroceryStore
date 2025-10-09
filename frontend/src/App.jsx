19e85d59458cef5d2709945762132321c9bae562
Subhash Kumar <subhashsonu7660@gmail.com>
2025-10-09 23:49:30 +0530

    Nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { CartProvider } from './CartContext'
 
 
 function App() {
 
   return (
+    <CartProvider>
    <Routes>
      <Route path="/" element={<Home/>} />
    </Routes>
+   </CartProvider>
   )
 }
 
