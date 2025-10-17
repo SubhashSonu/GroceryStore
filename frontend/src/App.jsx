8d9084fee245e7d79c6afda692ff48d5dc669fe9
Subhash Kumar <subhashsonu7660@gmail.com>
2025-10-17 16:12:47 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import { Route,Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route,Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { CartProvider } from './CartContext'
+
+const ScrollToTop = () =>{
+  const {pathname} = useLocation();
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[pathname]);
+  return null;
+}
 
 
 function App() {
 
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    Boolean(localStorage.getItem('authToken'))
+  )
+
+  useEffect(()=>{
+    const handler = ()=>{
+      setIsAuthenticated(Boolean(localStorage.getItem('authToken')))
+    }
+    window.addEventListener('authStateChanged',handler)
+    return () => window.removeEventListener('authStateChanged', handler)
+  },[])
+
   return (
     <CartProvider>
+      <ScrollToTop/>
    <Routes>
      <Route path="/" element={<Home/>} />
    </Routes>
