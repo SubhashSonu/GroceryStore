1c754d4f3b2b7b1f41a59dac244cc9ebf4ca0b88
Subhash Kumar <subhashsonu7660@gmail.com>
2025-10-17 16:12:57 +0530

    Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { footerStyles } from "../assets/dummyStyles";
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
-import { FiLink } from "react-icons/fi";
+import {
+  FaApplePay,
+  FaCcAmex,
+  FaCcMastercard,
+  FaCcPaypal,
+  FaCcVisa,
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaMapMarkedAlt,
+  FaPhone,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { FiBookmark, FiLink, FiMail } from "react-icons/fi";
+import { BsTelephone } from "react-icons/bs";
+import {BiMailSend} from "react-icons/bi";
 
 FaFacebook;
 
@@ -83,20 +98,131 @@ const Footer = () => {
               {["Home", "Items", "Contact"].map((item, idx) => (
                 <li key={idx}>
                   <a
-                    href={`/${item.toLowerCase()}`}
+                    href={item === 'Home' ? '/' : `/${item.toLowerCase()}`}
                     className={footerStyles.linkItem}
                   >
-                    <span className={footerStyles.linkBullet}>
-                     
-                    </span>
-                     {item}
+                    <span className={footerStyles.linkBullet}></span>
+                    {item}
                   </a>
                 </li>
               ))}
             </ul>
           </div>
+
+          {/* Contact Info */}
+          <div>
+            <h3 className={footerStyles.sectionTitle}>
+              <BsTelephone className={footerStyles.sectionIcon} />
+              Contact Us
+            </h3>
+            <ul className="space-y-4 text-sm sm:text-base">
+              <li className={footerStyles.contactItem}>
+                <div className={footerStyles.contactIconContainer}>
+                  <FaMapMarkedAlt className={footerStyles.contactIcon} />
+                </div>
+
+                <div>
+                  <p>22 Connaught Place, New Delhi, Delhi 110001, India</p>
+
+                  {/* Embedded Google Map */}
+                  <iframe
+                    title="Map Location - FreshCart Delhi"
+                    width="100%"
+                    height="200"
+                    loading="lazy"
+                    allowFullScreen
+                    className="rounded-lg mt-3"
+                    src="https://www.google.com/maps?q=Connaught+Place,+New+Delhi,+Delhi+110001,+India&output=embed"
+                  ></iframe>
+                </div>
+              </li>
+
+              <li className={footerStyles.contactItem}>
+                <div className={footerStyles.contactIconContainer}>
+                  <FaPhone className={footerStyles.contactIcon} />
+                </div>
+                <div>
+                  <p>+918584388489</p>
+                </div>
+              </li>
+
+              <li className={footerStyles.contactItem}>
+                <div className={footerStyles.contactIconContainer}>
+                  <FaEnvelope className={footerStyles.contactIcon} />
+                </div>
+                <div>
+                  <p>subh.say99@gmail.com</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          {/* Newsletter */}
+
+          <div>
+             <h3 className={footerStyles.sectionTitle}>
+              <FiMail className={footerStyles.sectionIcon}/>NewsLetter
+             </h3>
+             <p className={footerStyles.newsletterText}>
+              Subscribe to our newsletter for fresh updates, exclusive offers,
+              and seasonal recipes!
+             </p>
+             
+             <div className={footerStyles.newsletterForm}>
+              <input type="email" placeholder="Enter Email Address" className={footerStyles.newsletterButton}/>
+              <button className={footerStyles.newsletterButton}>
+                <BiMailSend className="mr-2 text-lg"/>
+                <span>Subscribe</span>
+              </button>
+             </div>
+
+             <p className={footerStyles.privacyText}>
+              We respect your privacy. Unsubscribe at any time.
+             </p>
+          </div>
+        </div>
+
+        {/* Payment Methods */}
+        <div className={footerStyles.paymentSection}>
+            <h4 className={footerStyles.paymentTitle}>
+              <FiBookmark className={footerStyles.paymentIcon}/> We Accept All Major payment
+            </h4>
+
+            <div className={footerStyles.paymentMethods}>
+              {[FaCcVisa, FaCcMastercard, FaCcPaypal, FaCcAmex, FaApplePay].map((Icon,idx)=>(
+                <div key={idx} className={footerStyles.paymentItem}>
+                  <Icon className={footerStyles.paymentIcon}/>
+                </div>
+              ))}
+
+            </div>
+        </div>
+
+        {/* HR */}
+        <div className={footerStyles.attribution}>
+          <div className={footerStyles.attributionBadge}>
+            <div className={footerStyles.hexagonContainer}>
+                
+                <div className={footerStyles.hexagon}></div>
+
+                <div className={footerStyles.hexagonInner}>
+                  <div className={footerStyles.hexagonInnerShape}></div>
+                </div>
+            </div>
+
+            <span className={footerStyles.attributionText}>
+             Designed By {' '}
+             <a href="https://github.com/SubhashSonu" className={footerStyles.attributionLink} target='_blank'>
+             Subhash Sonu
+             </a>
+             </span>
+          </div>
+
         </div>
       </div>
+      <style>
+           {footerStyles.customCSS}
+      </style>
     </footer>
   );
 };
