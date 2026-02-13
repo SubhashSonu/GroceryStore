b6c67b68d8b30185a9627de9bba2f4c2a386262f
Subhash Kumar <subhashsonu7660@gmail.com>
2026-02-14 00:54:29 +0530

    changes

--- frontend/src/pages/Items.jsx
+++ frontend/src/pages/Items.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Item from '../components/item'
+import Item from '../components/Item'
 
 
 
